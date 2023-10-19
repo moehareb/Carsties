4bca913db1a79edfe96410ff654e8b1700805e6a
Mohammad Harb <Moe@beytek.co>
2023-10-19 10:22:38 +0300

    end of section3

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitiazer.InitDb(app);
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
